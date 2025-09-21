e1bbd50a5590af866a1e9eefa3d705150bc79a82
kless <maksefimchick@gmail.com>
2025-09-21 23:46:56 +0300

    Update integration tests

--- user-service/src/test/java/com/internship/user_service/integration/CardServiceIntegrationTest.java
+++ user-service/src/test/java/com/internship/user_service/integration/CardServiceIntegrationTest.java
@@ -11,14 +11,26 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.context.jdbc.Sql;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
 
 import java.time.LocalDate;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
+@SpringBootTest
+@Testcontainers
+@ActiveProfiles("test")
 @DisplayName("Integration Tests for CardServiceImpl")
-class CardServiceIntegrationTest extends BaseIntegrationTest {
+class CardServiceIntegrationTest {
 
     @Autowired
     private CardInfoService cardInfoService;
@@ -28,6 +40,34 @@ class CardServiceIntegrationTest extends BaseIntegrationTest {
 
     private UserResponseDTO testUser;
     private CardInfoRequestDTO testCardRequest;
+
+    @Container
+    private static GenericContainer<?> redisContainer = new GenericContainer<>(
+            DockerImageName.parse("redis:7-alpine"))
+            .withExposedPorts(6379)
+            .withReuse(false);
+
+    @Container
+    private static PostgreSQLContainer<?> postgreSQLContainer = new PostgreSQLContainer<>("postgres:15-alpine")
+            .withDatabaseName("internship_test2")
+            .withUsername("postgres")
+            .withPassword("postgres")
+            .withReuse(false);
+
+    @DynamicPropertySource
+    static void configureProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgreSQLContainer::getJdbcUrl);
+        registry.add("spring.datasource.username", postgreSQLContainer::getUsername);
+        registry.add("spring.datasource.password", postgreSQLContainer::getPassword);
+
+        registry.add("spring.data.redis.host", redisContainer::getHost);
+        registry.add("spring.data.redis.port", () -> redisContainer.getMappedPort(6379));
+        registry.add("spring.data.redis.timeout", () -> "10000ms");
+
+        registry.add("spring.liquibase.enabled", () -> "false");
+
+        registry.add("spring.jpa.hibernate.ddl-auto", () -> "create-drop");
+    }
 
     @BeforeEach
     void setUp() {
