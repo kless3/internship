3ca5105994d320929bda4dcdc58c1f6828de3177
kless <maksefimchick@gmail.com>
2025-09-21 18:36:52 +0300

    Try to configure integration tests

--- user-service/src/test/java/com/internship/user_service/integration/BaseIntegrationTest.java
+++ user-service/src/test/java/com/internship/user_service/integration/BaseIntegrationTest.java
@@ -4,8 +4,11 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
 
 @SpringBootTest
 @Testcontainers
@@ -15,11 +18,9 @@ public abstract class BaseIntegrationTest {
     static PostgreSQLContainer<?> postgreSQLContainer = new PostgreSQLContainer<>("postgres:15-alpine")
             .withDatabaseName("internship")
             .withUsername("postgres")
-            .withPassword("postgres");
+            .withPassword("postgres")
+            .withReuse(true);
 
-    static {
-        postgreSQLContainer.start();
-    }
 
     @DynamicPropertySource
     static void postgresqlProperties(DynamicPropertyRegistry registry) {
@@ -27,4 +28,32 @@ public abstract class BaseIntegrationTest {
         registry.add("spring.datasource.password", postgreSQLContainer::getPassword);
         registry.add("spring.datasource.username", postgreSQLContainer::getUsername);
     }
+
+    @Container
+    public static GenericContainer<?> redisContainer =
+            new GenericContainer<>(DockerImageName.parse("redis:7-alpine"))
+                    .withExposedPorts(6379)
+                    .withReuse(true);
+
+    static {
+        if (!postgreSQLContainer.isRunning()) {
+            postgreSQLContainer.start();
+        }
+        if (!redisContainer.isRunning()) {
+            redisContainer.start();
+        }
+    }
+
+    @DynamicPropertySource
+    static void configureProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgreSQLContainer::getJdbcUrl);
+        registry.add("spring.datasource.username", postgreSQLContainer::getUsername);
+        registry.add("spring.datasource.password", postgreSQLContainer::getPassword);
+
+        registry.add("spring.data.redis.host", redisContainer::getHost);
+        registry.add("spring.data.redis.port", () -> redisContainer.getMappedPort(6379).toString());
+    }
+
+
+
 }
