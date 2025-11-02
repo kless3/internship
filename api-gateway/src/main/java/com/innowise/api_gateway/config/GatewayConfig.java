d0f9d4c57b10ba9877a16b7bb89ac0c1be9feb75
kless <maksefimchick@gmail.com>
2025-11-02 12:38:05 +0300

    Update gateway

--- api-gateway/src/main/java/com/innowise/api_gateway/config/GatewayConfig.java
+++ api-gateway/src/main/java/com/innowise/api_gateway/config/GatewayConfig.java
@@ -1,35 +1,35 @@
 package com.innowise.api_gateway.config;
 
+import com.innowise.api_gateway.property.ServiceProperties;
 import com.innowise.api_gateway.security.JwtGlobalFilter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@RequiredArgsConstructor
 public class GatewayConfig {
 
     private final JwtGlobalFilter jwtFilter;
-
-    public GatewayConfig(JwtGlobalFilter jwtFilter) {
-        this.jwtFilter = jwtFilter;
-    }
+    private final ServiceProperties serviceProperties;
 
     @Bean
     public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
         return builder.routes()
                 .route("auth-service", r -> r
                         .path("/api/v1/auth/**")
-                        .uri("http://localhost:8081"))
+                        .uri(serviceProperties.getAuthService()))
                 .route("order-service", r -> r
                         .path("/api/v1/orders/**")
-                        .uri("http://localhost:8082"))
+                        .uri(serviceProperties.getOrderService()))
                 .route("user-service-public", r -> r
                         .path("/api/v1/users")
-                        .uri("http://localhost:8080"))
+                        .uri(serviceProperties.getUserService()))
                 .route("user-service-secured", r -> r
                         .path("/api/v1/users/**")
-                        .uri("http://localhost:8080"))
+                        .uri(serviceProperties.getUserService()))
                 .build();
     }
 }
