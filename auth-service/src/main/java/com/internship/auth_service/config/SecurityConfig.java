14fcaf9d7e3ac72a087afb24aa13ebe77313b565
kless <maksefimchick@gmail.com>
2025-11-30 16:12:51 +0300

    Add k8s

--- auth-service/src/main/java/com/internship/auth_service/config/SecurityConfig.java
+++ auth-service/src/main/java/com/internship/auth_service/config/SecurityConfig.java
@@ -37,7 +37,9 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(
                                 "/api/v1/auth/login",
-                                "/api/v1/auth/register"
+                                "/api/v1/auth/register",
+                                "/actuator/**",
+                                "/actuator/health/**"
                         ).permitAll()
                         .anyRequest()
                         .authenticated())
